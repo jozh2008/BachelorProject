b8daa46425e29d61a89182895ca65c9db694a2e5
jozh2008 <zwerg9916@gmail.com>
2023-11-01 14:25:05 +0100

    New methods

---
+++ save.py
@@ -0,0 +1,13 @@
+        input_id["results"] = [{'__index__': 0,
+                                'software_cond': {'__current_case__': 0,
+                                                  'values': [{'id': '4838ba20a6d867651787f6001f11eacc',
+                                                                        'src': 'hda'}],
+                                                  'software': 'fastqc'}}]
+
+
+
+
+                        {
+                            'src': 'hda',
+                            'id': input_data_id_2 # Replace with the actual input data ID
+                        },
